2a00a19ea3be9266a52f1f93b30845d45b68eeb8
Nick Yang <nick10811@gmail.com>
2021-12-15 10:04:10 +0800

    Consider 2nd category in Factory

--- DynamicTable/Factory/ComponentFactory.swift
+++ DynamicTable/Factory/ComponentFactory.swift
@@ -8,20 +8,22 @@
 import Foundation
 import UIKit
 
-// MARK: - Factory Pattern
 enum PageType: CustomStringConvertible {
     case page1, page2, page3, page4
-    
-    var getComponents: [ComponentType] {
+
+    // MARK: Factory Pattern
+    var getPageLayout: [ComponentType] {
         switch self {
-        case .page1: return [.component1, .component2, .component3, .component4]
-        case .page2: return [.component4, .component3, .component2]
-        case .page3: return [.component2, .component2]
-        case .page4: return [.component3, .component3, .component1]
+        case .page1: return [.component1(), .component2(), .triggerComponent(),    // 1st category
+                             .component1(1), .component1(1), .triggerComponent(1), // 2nd category
+                             .component4(2)]                                       // 3rd category
+        case .page2: return [.component4(), .component3(), .component2()]
+        case .page3: return [.component2(), .component2()]
+        case .page4: return [.component3(), .component3(), .component1()]
         }
     }
     
-    // vc title
+    // page title
     var description: String {
         switch self {
         case .page1: return "Page 1"
@@ -33,8 +35,14 @@ enum PageType: CustomStringConvertible {
     
 }
 
-enum ComponentType {
-    case component1, component2, component3, component4
+enum ComponentType: CustomStringConvertible {
+    case component1(_ category: Int = 0),
+         component2(_ category: Int = 0),
+         component3(_ category: Int = 0),
+         component4(_ category: Int = 0),
+         component5(_ category: Int = 0),
+         component6(_ category: Int = 0),
+         triggerComponent(_ category: Int = 0)
     
     var backgroundColor: UIColor {
         switch self {
@@ -42,6 +50,39 @@ enum ComponentType {
         case .component2: return .blue
         case .component3: return .green
         case .component4: return .orange
+        case .triggerComponent: return .red
+        default: return .clear
+        }
+    }
+    
+    var category: Int {
+        switch self {
+        case .component1(let num): return num
+        case .component2(let num): return num
+        case .component3(let num): return num
+        case .component4(let num): return num
+        case .component5(let num): return num
+        case .component6(let num): return num
+        case .triggerComponent(let num): return num
+        }
+    }
+    
+    var hasNextCategory: Bool {
+        switch self {
+        case .triggerComponent: return true
+        default: return false
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .component1: return "Component 1"
+        case .component2: return "Component 2"
+        case .component3: return "Component 3"
+        case .component4: return "Component 4"
+        case .component5: return "Component 5"
+        case .component6: return "Component 6"
+        case .triggerComponent: return "I have next category"
         }
     }
 }
